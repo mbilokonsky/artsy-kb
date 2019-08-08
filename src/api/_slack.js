1260440bf555183d4f263ea1a09308f6a6496429
mbilokonsky <mbilokonsky@gmail.com>
2019-08-08 13:39:37 -0400

    cowboy coding ahoy

--- src/api/_slack.js
+++ src/api/_slack.js
@@ -2,8 +2,7 @@ module.exports = async (req, res) => {
   console.log("SLACK ENDPOINT HIT!");
 
   const { body } = req;
-  const payload = JSON.parse(body);
-  const json = JSON.stringify(payload, null, 2);
+  const json = JSON.stringify(body, null, 2);
   console.log(json);
 
   const output = {
