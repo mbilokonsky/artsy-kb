8659973e5d55f2bc68e2205e7880bd8888a11fd2
mbilokonsky <mbilokonsky@gmail.com>
2019-08-08 13:46:41 -0400

    calamity

--- src/api/_slack.js
+++ src/api/_slack.js
@@ -2,7 +2,7 @@ module.exports = async (req, res) => {
   console.log("SLACK ENDPOINT HIT!");
 
   const { body } = req;
-  const json = JSON.stringify(body, null, 2);
+  const json = JSON.stringify(req, null, 2);
   console.log(json);
 
   const output = {
