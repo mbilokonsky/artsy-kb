d9d11a9c4f6ef1f681805efecfbc45f1481a43e8
mbilokonsky <mbilokonsky@gmail.com>
2019-08-08 13:36:52 -0400

    fixing git history I hope

--- src/api/_slack.js
+++ src/api/_slack.js
@@ -1,13 +1,14 @@
 module.exports = async (req, res) => {
   console.log("SLACK ENDPOINT HIT!");
 
-  const { body } = req.body;
-
-  console.log(body);
+  const { body } = req;
+  const payload = JSON.parse(body);
+  const json = JSON.stringify(payload, null, 2);
+  console.log(json);
 
   const output = {
     response_type: "ephemeral",
-    text: "KB server sees you!"
+    text: json
   };
 
   res.json(output);
