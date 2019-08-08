3b57542f8e8d46e9cf8c3491238ac2d736bc3098
xtina-starr <c.thompsonms@gmail.com>
2019-08-08 15:58:10 -0400

    display options when user enters kb command

--- src/api/_slack.js
+++ src/api/_slack.js
@@ -1,15 +1,59 @@
 module.exports = async (req, res) => {
   console.log("SLACK ENDPOINT HIT!");
 
-  const { body } = req;
+  const {
+    body
+  } = req;
   const json = JSON.stringify(body, null, 2);
   console.dir(req);
   console.log("req keys:", Object.keys(req).join(", "));
 
   const output = {
     response_type: "ephemeral",
-    text: json
+    block
   };
 
   res.json(output);
 };
+
+const block = [{
+    "type": "section",
+    "block_id": "section789",
+    "fields": [{
+      "type": "mrkdwn",
+      "text": "Welcome to Artsy knowledge base!"
+    }]
+  },
+  {
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": "Pick an item from the dropdown list"
+    },
+    "accessory": {
+      "type": "static_select",
+      "placeholder": {
+        "type": "plain_text",
+        "text": "Select an item",
+        "emoji": true
+      },
+      "options": [{
+          "text": {
+            "type": "plain_text",
+            "text": "Ask a question",
+            "emoji": true
+          },
+          "value": "askQuestion"
+        },
+        {
+          "text": {
+            "type": "plain_text",
+            "text": "Search by tag",
+            "emoji": true
+          },
+          "value": "searchByTag"
+        },
+      ]
+    }
+  },
+]
